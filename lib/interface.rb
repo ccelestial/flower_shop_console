433d4c81af01b54bf87c079b0f52fee3824bdea0
ccelestial <celestialchristopher@gmail.com>
2018-01-11 22:45:06 +0800

    Added input from user in interface

--- lib/interface.rb
+++ lib/interface.rb
@@ -1,15 +1,32 @@
 require_relative 'product_factory'
+require_relative 'order'
 
 class Interface
   def initialize
     @products = ProductFactory.get
+    @order = Order.new
   end
 
   def open
     puts products_display
+
+    get_order_items
+
+    puts @order.items.map(&:product_code)
   end
 
   private
+
+  def get_order_items
+    puts "Enter quantity + code: (e.g. 10 R12): Finish by typing \"x\"."
+    loop do
+      input = gets.chomp
+
+      @order.add_item(input)
+
+      break if input.downcase == "x"
+    end
+  end
 
   def products_display
     view = ["Welcome to FlowerShop Console App\n\nPRODUCTS:\nNAME\tCODE \tBUNDLES\n"]
--- spec/lib/interface_spec.rb
+++ spec/lib/interface_spec.rb
@@ -1,18 +1,40 @@
 require 'spec_helper'
 require 'interface'
+require "stringio"
 
 describe Interface do
   describe '.new' do
     before { allow(ProductFactory).to receive(:get) }
+    before { allow(Order).to receive(:new) }
 
     it 'creates an instance of Interface' do
       interface = Interface.new
       expect(interface).to be_an_instance_of(Interface)
     end
 
-    it 'creates products instance variable' do
+    it 'calls ProductFactory to get products' do
       expect(ProductFactory).to receive(:get)
       Interface.new
+    end
+
+    it 'creates new Order' do
+      expect(Order).to receive(:new)
+      Interface.new
+    end
+  end
+
+  describe '.open' do
+    before { allow(ProductFactory).to receive(:get).and_return([]) }
+    before { allow_any_instance_of(Product).to receive(:name) }
+    before { allow_any_instance_of(Product).to receive(:code) }
+    before { allow_any_instance_of(Product).to receive(:bundles).and_return([]) }
+    before { allow_any_instance_of(Order).to receive(:add_item) }
+    before { allow_any_instance_of(Interface).to receive(:gets).and_return("10 R12", "x") }
+    let(:interface) { Interface.new }
+
+    it 'calls Order.add_item when inputing order' do
+      expect_any_instance_of(Order).to receive(:add_item)
+      interface.open
     end
   end
 end
